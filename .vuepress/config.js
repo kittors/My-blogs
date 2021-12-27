cb2643e558aaf2773868ec3c6263fc983140ad03
kittors <364299311@qq.com>
2021-12-27 10:15:26 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -20,7 +20,7 @@ module.exports = {
       { text: 'Contact', 
         icon: 'reco-message',
         items: [
-          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' }
+          { text: 'GitHub', link: 'https://github.com/kittors', icon: 'reco-github' }
         ]
       }
     ],
@@ -67,13 +67,13 @@ module.exports = {
     // 最后更新时间
     lastUpdated: 'Last Updated',
     // 作者
-    author: 'reco_luan',
+    author: 'Kittors',
     // 作者头像
-    authorAvatar: '/avatar.png',
+    authorAvatar: '/avatar.jpg',
     // 备案号
     record: 'xxxx',
     // 项目开始时间
-    startYear: '2017'
+    startYear: '2021'
     /**
      * 密钥 (if your blog is private)
      */
